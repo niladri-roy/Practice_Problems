69024e13b44217f907d8032b75cbd3175d4b23c4
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2022-09-04 23:32:50 +0530

    Day 1 (4th September, 2022)

--- Reverse a Given Array/solution.cpp
+++ Reverse a Given Array/solution.cpp
@@ -1,24 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print()
+void print(int arrayToPrint[], int n)
 {
-    
+    for(int i=0; i<n; i++){
+        cout << arrayToPrint[i] << " ";
+    }
+    cout << endl;
 }
 
-int approach1(int arr[], int n)
+void approach1(int arr[], int n)
 {
+    int ans[n];
+    for(int i=0; i<n; i++){
+        ans[n-i-1] = arr[i];
+    }
+    print(ans, n);
+}
+
+void approach2(int arr[], int n)
+{
+    int start = 0;
+    int end = n-1;
+
+    int ans[n];
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = arr[i];
+    }
+
+    while(start <= end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    print(arr, n);
 
 }
 
-int approach2(int arr[], int n)
+void approach3(int arr[], int start, int end, int n)
 {
+    if(start > end)
+    {
+        print(arr, n);
+        return;
+    }
 
-}
-
-int approach3(int arr[], int n)
-{
-
+    swap(arr[start], arr[end]);
+    return approach3(arr, start+1, end-1, n);
 }
 
 int main()
@@ -34,10 +63,21 @@ int main()
     {
         cin >> arr[i];
     }
-    int ans1 = approach1(arr, n);
-    int ans2 = approach2(arr, n);
 
-    cout << "The largest element: " << endl;
-    cout << "According to Approach 1: " << ans1 << endl;
-    cout << "According to Approach 2: " << ans2 << endl;
+    int ans[n];
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = arr[i];
+    }
+    
+    cout << "The reversed array: " << endl;
+
+    cout << "According to approach 1 :" << endl;
+    approach1(arr, n);
+
+    cout << "According to approach 2 :" << endl;
+    approach2(arr, n);
+
+    cout << "According to approach 3 :" << endl;
+    approach3(ans, 0, n-1, n);
 }
