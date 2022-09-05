0c1c114ae1a70e505d0b7e49bb99950415da3b63
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2022-09-05 09:42:59 +0530

    Day 2 (5th September, 2022)

--- Reverse Integer/solution.cpp
+++ Reverse Integer/solution.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+/*This problem is from leetcode*/
+
+#include<bits/stdc++.h>
 using namespace std;
+
+int reverse(int number)
+{
+    int rem = 0;
+    int digit;
+
+    while(number!=0){
+
+        digit = number%10;
+        if(rem > INT_MAX/10 || rem < INT_MIN/10){
+            return 0;
+        }
+
+        rem = (rem * 10) + digit;
+        number = number/10;
+    }
+    return rem;
+}
 
 int main()
 {
-    
+    int number;
+    cout << "Enter a number which need to be reversed: ";
+    cin >> number;
+
+    int ans = reverse(number);
+    cout << "The number you have entered : " << number <<  endl;
+    cout << "The number after reversed : " << ans << endl;
 }
